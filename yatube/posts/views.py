35991f47e6630abeae5c1b7e908a108ac9f8ced7
Nick Zasimov <zasimovnikolai@gmail.com>
2023-01-24 01:00:20 +0300

    Добавил группы по заданию

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post, Group
 
 
 def index(request):
@@ -11,9 +13,10 @@ def index(request):
 
 
 def group_posts(request, slug):
-    template = 'posts/group_list.html'
-    title = "Здесь будет информация о группах проекта Yatube"
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'title': title
+        'group': group,
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/group_list.html', context)
